d3adb0cf33a662b778ae3eb4a857275916417729
MarcosAurelioBR <155568956+MarcosAurelioBR@users.noreply.github.com>
2026-02-14 16:22:41 -0300

    Update 01_clean_transactions.sql

--- 01_exploration/02_cleaning/01_clean_transactions.sql
+++ 01_exploration/02_cleaning/01_clean_transactions.sql
@@ -1,1 +1,28 @@
+/* PROJETO: Análise de Ecossistema de Pagamentos
+   OBJETIVO: Sanitize e tipagem da tabela de transações (15M+ linhas).
+*/
 
+CREATE OR ALTER VIEW vw_transactions_cleaned AS
+SELECT 
+    CAST(id AS BIGINT) AS transaction_id,
+    CAST(client_id AS INT) AS user_id,
+    CAST(card_id AS INT) AS card_id,
+    
+    -- Conversão de Data: Texto para Datetime
+    CAST([date] AS DATETIME) AS transaction_timestamp,
+    
+    -- Limpeza de Moeda: Remove '$' e converte para DECIMAL(18,2)
+    CAST(REPLACE(REPLACE(amount, '$', ''), ',', '') AS DECIMAL(18,2)) AS amount_value,
+    
+    use_chip,
+    merchant_city,
+    merchant_state,
+    mcc,
+    
+    -- Padronização de Erros: Vazio vira 'Success'
+    CASE 
+        WHEN errors = '' OR errors IS NULL THEN 'Success'
+        ELSE errors 
+    END AS transaction_status
+FROM payments_core.dbo.transactions_raw;
+GO
