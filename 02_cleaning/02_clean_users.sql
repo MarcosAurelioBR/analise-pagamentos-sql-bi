5c9d6a53ccb5b07768288be78bedddfb15a1beb5
MarcosAurelioBR <155568956+MarcosAurelioBR@users.noreply.github.com>
2026-02-18 21:43:39 -0300

    Update 02_clean_users.sql

--- 02_cleaning/02_clean_users.sql
+++ 02_cleaning/02_clean_users.sql
@@ -1,24 +1,22 @@
 /* PROJETO: Análise de Ecossistema de Pagamentos
    OBJETIVO: Limpeza de dados demográficos e financeiros dos clientes.
 */
-
 CREATE OR ALTER VIEW vw_users_cleaned AS
-SELECT 
-    CAST(user_id AS INT) AS user_id,
-    current_age,
-    gender,
-    credit_score,
-    
-    -- Limpeza de Renda e Dívida
-    CAST(REPLACE(REPLACE(yearly_income, '$', ''), ',', '') AS DECIMAL(18,2)) AS yearly_income,
-    CAST(REPLACE(REPLACE(total_debt, '$', ''), ',', '') AS DECIMAL(18,2)) AS total_debt,
-    
-    -- Feature Engineering: Taxa de Endividamento
+WITH base AS (
+    SELECT
+        CAST(user_id AS INT) AS user_id,
+        current_age,
+        gender,
+        credit_score,
+        CAST(REPLACE(REPLACE(yearly_income, '$', ''), ',', '') AS DECIMAL(18,2)) AS yearly_income,
+        CAST(REPLACE(REPLACE(total_debt, '$', ''), ',', '') AS DECIMAL(18,2)) AS total_debt
+    FROM payments_core.dbo.users_raw
+)
+
+SELECT *,
     CASE 
-        WHEN CAST(REPLACE(REPLACE(yearly_income, '$', ''), ',', '') AS DECIMAL(18,2)) > 0 
-        THEN CAST(REPLACE(REPLACE(total_debt, '$', ''), ',', '') AS DECIMAL(18,2)) / 
-             CAST(REPLACE(REPLACE(yearly_income, '$', ''), ',', '') AS DECIMAL(18,2))
-        ELSE 0 
+        WHEN yearly_income > 0 
+        THEN total_debt / yearly_income
+        ELSE 0
     END AS debt_to_income_ratio
-FROM payments_core.dbo.users_raw;
-GO
+FROM base;
